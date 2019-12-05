672215cccedbf03c6fb29b5683ed7874fe594e5a
sunyard <hanr.wang@sunyard.com>
2019-12-05 17:27:09 +0800

    第一个版本

---
+++ zhixuejava/service-ribbon/src/main/java/com/zhixuejava/cloud/ServiceRibbonApplication.java
@@ -0,0 +1,28 @@
+package com.zhixuejava.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ *  cloud 负载均衡
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
