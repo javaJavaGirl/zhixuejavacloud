82364ba9f317f1c3aabe1de17860d125afdab144
sunyard <hanr.wang@sunyard.com>
2019-12-12 15:05:19 +0800

    1.网管路由 2.config-sever的配置

---
+++ service-zuul/src/main/java/com/zhixuejava/cloud/MyFilter.java
@@ -0,0 +1,56 @@
+package com.zhixuejava.cloud;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+    @Override
+    public String filterType() {
+//        filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+//        pre：路由之前
+//        routing：路由之时
+//        post： 路由之后
+//        error：发送错误调用
+
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
---
+++ service-zuul/src/main/java/com/zhixuejava/cloud/ServiceZuulApplication.java
@@ -0,0 +1,20 @@
+package com.zhixuejava.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * ApI网络服务
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
